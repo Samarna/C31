dc58325a8de2661d7d9a196898028d00188b97bc
Samarna <63645732+Samarna@users.noreply.github.com>
2020-05-24 12:57:45 +0530

    Update Target.js

--- Target.js
+++ Target.js
@@ -1,1 +1,20 @@
+class Box {
+    constructor(x, y) {
+      var options = {
+          'density':0.5
+      }
+      this.body = Bodies.rectangle(x, y, width, height,options);
 
+      this.width = 10;
+      this.height = 10;
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y);
+      rectMode(CENTER);
+      fill("lightgrey");
+      rect(0,0, 10, 10);
+      pop();
+    }
+  };
